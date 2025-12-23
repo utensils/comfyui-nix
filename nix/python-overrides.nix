34322226420631a371a465e5ab01ada5187e5cc3
James Brink <brink.james@gmail.com>
2025-12-23 15:07:00 -0700

    Align CUDA python deps with torchWithCuda

--- nix/python-overrides.nix
+++ nix/python-overrides.nix
@@ -5,32 +5,52 @@
 }:
 let
   lib = pkgs.lib;
+  useCuda = cudaSupport && pkgs.stdenv.isLinux;
 in
 final: prev:
-lib.optionalAttrs (prev ? torch) {
-  spandrel = final.buildPythonPackage rec {
-    pname = "spandrel";
-    version = versions.vendored.spandrel.version;
-    format = "wheel";
-    src = pkgs.fetchurl {
-      url = versions.vendored.spandrel.url;
-      hash = versions.vendored.spandrel.hash;
+lib.optionalAttrs (prev ? torch) (
+  let
+    torchForCuda = if useCuda && prev ? torchWithCuda then prev.torchWithCuda else final.torch;
+  in
+  {
+    spandrel = final.buildPythonPackage rec {
+      pname = "spandrel";
+      version = versions.vendored.spandrel.version;
+      format = "wheel";
+      src = pkgs.fetchurl {
+        url = versions.vendored.spandrel.url;
+        hash = versions.vendored.spandrel.hash;
+      };
+      dontBuild = true;
+      dontConfigure = true;
+      nativeBuildInputs = [
+        final.setuptools
+        final.wheel
+        final.ninja
+      ];
+      propagatedBuildInputs =
+        lib.optionals (prev ? torch) [ torchForCuda ]
+        ++ lib.optionals (prev ? torchvision) [ final.torchvision ]
+        ++ lib.optionals (prev ? safetensors) [ final.safetensors ]
+        ++ lib.optionals (prev ? numpy) [ final.numpy ]
+        ++ lib.optionals (prev ? einops) [ final.einops ]
+        ++ lib.optionals (prev ? typing-extensions) [ final.typing-extensions ];
+      pythonImportsCheck = [ ];
+      doCheck = false;
     };
-    dontBuild = true;
-    dontConfigure = true;
-    nativeBuildInputs = [
-      final.setuptools
-      final.wheel
-      final.ninja
-    ];
-    propagatedBuildInputs =
-      lib.optionals (prev ? torch) [ final.torch ]
-      ++ lib.optionals (prev ? torchvision) [ final.torchvision ]
-      ++ lib.optionals (prev ? safetensors) [ final.safetensors ]
-      ++ lib.optionals (prev ? numpy) [ final.numpy ]
-      ++ lib.optionals (prev ? einops) [ final.einops ]
-      ++ lib.optionals (prev ? typing-extensions) [ final.typing-extensions ];
-    pythonImportsCheck = [ ];
-    doCheck = false;
-  };
-}
+  }
+  // lib.optionalAttrs (useCuda && prev ? torchWithCuda) (
+    (lib.optionalAttrs (prev ? torchvision) {
+      torchvision = prev.torchvision.override { torch = prev.torchWithCuda; };
+    })
+    // (lib.optionalAttrs (prev ? torchaudio) {
+      torchaudio = prev.torchaudio.override { torch = prev.torchWithCuda; };
+    })
+    // (lib.optionalAttrs (prev ? torchsde) {
+      torchsde = prev.torchsde.override { torch = prev.torchWithCuda; };
+    })
+    // (lib.optionalAttrs (prev ? kornia) {
+      kornia = prev.kornia.override { torch = prev.torchWithCuda; };
+    })
+  )
+)
